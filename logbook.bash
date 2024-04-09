94a9ded460f5f3f402c4605f7652d26fc86ec7de
TM90 <tobias_markus@gmx.net>
2024-04-09 17:52:32 +0200

    updated bash command to really create keybinding

--- logbook.bash
+++ logbook.bash
@@ -1,3 +1,5 @@
 alias logbook=${LOGBOOK_PATH}/logbook/logbook
 PROMPT_COMMAND='logbook add -command "$(history 1)"'
+bind -m emacs-standard -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
 bind -m vi-command -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
+bind -m vi-insert -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
