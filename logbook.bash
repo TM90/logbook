b9e590af2cea42c109fefcfff67f7bcf87599435
Tobias Markus <tobias.markus@extoll.de>
2024-04-09 14:31:09 +0200

    initial working version of the bash setup script

--- logbook.bash
+++ logbook.bash
@@ -1,2 +1,3 @@
-val=$(history 1)
-~/Playground/golang/logbook/logbook add -command "$val"
+alias logbook=${LOGBOOK_PATH}/logbook/logbook
+PROMPT_COMMAND='logbook add -command "$(history 1)"'
+bind -m vi-command -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
