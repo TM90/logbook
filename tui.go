14821ec5d956eec31a796c6d76993d60f57c3ae7
TM90 <tobias_markus@gmx.net>
2024-05-04 16:41:29 +0200

    added page and display size to model render list of commands dynamically based on terminal size

--- tui.go
+++ tui.go
@@ -8,8 +8,10 @@ import (
 )
 
 type model struct {
-	logbook []logbookEntry
-	cursor  int
+	logbook            []logbookEntry
+	cursor             int
+	logbookDisplaySize int
+	page               int
 }
 
 func initialModel() model {
@@ -40,27 +42,55 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+
+	case tea.WindowSizeMsg:
+		m.logbookDisplaySize = msg.Height - 5
+		db, err := dbOpen()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		rows, err := db.Query(fmt.Sprintf("SELECT * FROM command GROUP BY command_name ORDER BY id DESC LIMIT 0,%d", m.logbookDisplaySize))
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		db.Close()
+		logbook := initLogBook(rows)
+		entries := []logbookEntry{}
+		for logbook.Next() {
+			entries = append(entries, logbook.Value())
+		}
+		m.logbook = entries
+		if m.cursor > m.logbookDisplaySize {
+			m.cursor = m.logbookDisplaySize - 1
+		}
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
-		case "up", "k":
+		case "up":
 			if m.cursor > 0 {
 				m.cursor--
 			}
-		case "down", "j":
-			if m.cursor < 20 {
+		case "down":
+			if m.cursor < m.logbookDisplaySize-1 {
 				m.cursor++
 			}
+		case "left":
+			if m.page > 0 {
+				m.page--
+			}
+		case "right":
+			m.page++
 		}
-
 	}
 
 	return m, nil
 }
 
 func (m model) View() string {
-	s := "Results: \n\n"
+	s := fmt.Sprintf("Results %d: \n\n", m.page)
 	for i, entry := range m.logbook {
 		cursor := " "
 		if m.cursor == i {
