3f2f1746ad6f5002b71d87ad31dacf659794f294
TM90 <tobias_markus@gmx.net>
2024-08-30 09:36:22 +0200

    modified query

--- logbook.bash
+++ logbook.bash
@@ -2,6 +2,6 @@
 alias logbook=${LOGBOOK_PATH}/logbook/logbook
 export LOGBOOK_UUID=$(uuidgen -r)
 PROMPT_COMMAND='logbook add --exit-code=$? --command="$(history 1)" --uuid=$LOGBOOK_UUID'
-bind -m emacs-standard -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
-bind -m vi-command -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
-bind -m vi-insert -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT * FROM command GROUP BY command_name ORDER BY id DESC" | fzf)"'
+bind -m emacs-standard -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
+bind -m vi-command -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
+bind -m vi-insert -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
