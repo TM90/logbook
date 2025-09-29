56c715fa13458daf9b5d0df4b359d7bd8514bda7
TM90 <tobias_markus@gmx.net>
2025-09-29 22:17:25 +0200

    output is now put into wl-copy

--- logbook.bash
+++ logbook.bash
@@ -1,7 +1,14 @@
+function __logbook_hook__() {
+  if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    wl-copy "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"
+  else
+    xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"
+  fi
+}
 
 alias logbook=${LOGBOOK_PATH}/logbook/logbook
 export LOGBOOK_UUID=$(uuidgen -r)
 PROMPT_COMMAND=('logbook add --exit-code=$? --command="$(history 1)" --uuid=$LOGBOOK_UUID' "${PROMPT_COMMAND[@]}")
-bind -m emacs-standard -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
-bind -m vi-command -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
-bind -m vi-insert -x '"\C-r": xdotool type "$(logbook raw_query --query "SELECT id, command_name, history_id, exit_code, uuid, MAX(exec_time) FROM command GROUP BY command_name ORDER BY exec_time DESC" | fzf)"'
+bind -m emacs-standard -x '"\C-r": __logbook_hook__'
+bind -m vi-command -x '"\C-r": __logbook_hook__'
+bind -m vi-insert -x '"\C-r": __logbook_hook__'
